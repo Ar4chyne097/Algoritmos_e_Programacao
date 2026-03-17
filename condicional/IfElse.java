924b2b58d44d9eae9136995fc92bb19182c9020c
Alice <paulorobertobernal68@gmail.com>
2026-03-17 14:26:44 -0300

    :3

---
+++ condicional/IfElse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class IfElse{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        float n1,n2,n3, média;
+
+        System.out.print("Digite a Primeira Nota:");
+        n1 = sc.nextFloat();
+        
+        System.out.print("Digite a Segunda Nota:");
+        n2 = sc.nextFloat();
+
+        System.out.print("Digite a Terceira Nota:");
+        n3 = sc.nextFloat();
+
+        média = ((n1*3)+(n2*3)+(n3*4))/10;
+
+        if(média >= 7.0){
+            System.out.println("Sua média foi: "+média + "\nAprovado!");
+        }else{
+            System.out.println("Sua média foi: "+média + "\nReprovado!");
+        }
+    }
+}
